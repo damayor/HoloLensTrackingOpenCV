b675a350f684679ff476e9a8a241da442a4099e3
damayor <dr.mayorga20@uniandes.edu.co>
2019-02-13 13:30:14 -0500

    Revert "imgproc PerspectiveTransform fix"
    This reverts commit d4cbb5ae7bc4d56e42c9c42811359aceca416ce3.

--- Assets/ARTrackingProject/OpenCV/MonoTrackingExample.cs
+++ Assets/ARTrackingProject/OpenCV/MonoTrackingExample.cs
@@ -334,7 +334,7 @@ namespace OpenCVForUnityExample
                 srcRectMat.put (0, 0, tl.x, tl.y, tr.x, tr.y, br.x, br.y, bl.x, bl.y);
                 dstRectMat.put (0, 0, 0, 0, quadScale.x, 0, quadScale.x, quadScale.y, 0, quadScale.y);            
 
-                using (Mat perspectiveTransform = Imgproc.getPerspectiveTransform (srcRectMat, dstRectMat)) //
+                using (Mat perspectiveTransform = Imgproc.getPerspectiveTransform (srcRectMat, dstRectMat))
                 using (MatOfPoint2f srcPointMat = new MatOfPoint2f (screenPoint))
                 using (MatOfPoint2f dstPointMat = new MatOfPoint2f ()) {
                     Core.perspectiveTransform (srcPointMat, dstPointMat, perspectiveTransform);
